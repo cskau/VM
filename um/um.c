2d2fa19bafccb3916b7a72d4b70c37374bc50aea
Clement Skau <clementskau@gmail.com>
2012-02-26 01:35:52 +0100

    Refactors UM to use 32 bit addresses as PA references

--- um/um.c
+++ um/um.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 /* Google C++ Style Guide : 
     http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
 */
 
 #define CODE_BUF_SIZE 32000000
-#define MEMO_BUF_SIZE 32000000
 
 /*
                           A     C
@@ -34,20 +34,20 @@
 #define OV_MASK 0x01FFFFFF
 
 /* OP codes */
-#define OP_IFM 0x00000000  // 0. Conditional Move.
-#define OP_ARI 0x10000000  // 1. Array Index.
-#define OP_ARA 0x20000000  // 2. Array Amendment.
-#define OP_ADD 0x30000000  // 3. Addition.
-#define OP_MUL 0x40000000  // 4. Multiplication.
-#define OP_DIV 0x50000000  // 5. Division.
-#define OP_NOT 0x60000000  // 6. Not-And.
-#define OP_HLT 0x70000000  // 7. Halt.
-#define OP_ALC 0x80000000  // 8. Allocation.
-#define OP_ABD 0x90000000  // 9. Abandonment.
-#define OP_OUT 0xA0000000  // 10. Output.
-#define OP_INP 0xB0000000  // 11. Input.
-#define OP_LDP 0xC0000000  // 12. Load Program.
-#define OP_ORT 0xD0000000  // 13. Orthography.
+#define OP_IFM 0x00000000  /* 0. Conditional Move. */
+#define OP_ARI 0x10000000  /* 1. Array Index. */
+#define OP_ARA 0x20000000  /* 2. Array Amendment. */
+#define OP_ADD 0x30000000  /* 3. Addition. */
+#define OP_MUL 0x40000000  /* 4. Multiplication. */
+#define OP_DIV 0x50000000  /* 5. Division. */
+#define OP_NOT 0x60000000  /* 6. Not-And. */
+#define OP_HLT 0x70000000  /* 7. Halt. */
+#define OP_ALC 0x80000000  /* 8. Allocation. */
+#define OP_ABD 0x90000000  /* 9. Abandonment. */
+#define OP_OUT 0xA0000000  /* 10. Output. */
+#define OP_INP 0xB0000000  /* 11. Input. */
+#define OP_LDP 0xC0000000  /* 12. Load Program. */
+#define OP_ORT 0xD0000000  /* 13. Orthography. */
 
 /*
   When reading programs from legacy "unsigned 8-bit character"
@@ -55,9 +55,9 @@
   'A' as the most magnificent byte, and 'D' as the most shoddy, with
   'B' and 'C' considered lovely and mediocre respectively.
 */
-unsigned int * LoadPlatterArrayOrDie(char *fname) {
-  unsigned int *pa;
-  unsigned int new_platter = 0, ch = 0, i = 0;
+uint32_t * LoadPlatterArrayOrDie(char *fname) {
+  uint32_t *pa;
+  uint32_t new_platter = 0, ch = 0, i = 0;
 
   FILE *file = fopen(fname, "r");
 
@@ -66,11 +66,10 @@ unsigned int * LoadPlatterArrayOrDie(char *fname) {
     exit(-1);
   }
 
-  pa = calloc(CODE_BUF_SIZE, sizeof(unsigned int));
+  /* leave entry 0 for storing size */
+  pa = &(((uint32_t*)calloc(CODE_BUF_SIZE, sizeof(uint32_t)))[1]);
 
-  while ((ch = getc(file)) != EOF) {
-    /* leave entry 0 for storing size */
-    i++;
+  for (i = 0; (ch = getc(file)) != EOF; i++) {
     new_platter = (ch << 24);
     if ((ch = getc(file)) != EOF) {
       new_platter |= (ch << 16);
@@ -90,29 +89,25 @@ unsigned int * LoadPlatterArrayOrDie(char *fname) {
     pa[i] = new_platter;
   }
 
-  /* store array size in entry 0 */
-  pa[0] = i;
+  /* store array size in first entry */
+  pa[-1] = i;
 
   fclose(file);
 
   return pa;
 }
 
-void SpinCycle(unsigned int *pa) {
-  unsigned int regs[] = {0, 0, 0, 0, 0, 0, 0, 0};
-  unsigned int **collection;
-  unsigned int ip = 0, pp = 0, pp_lb = 0;
+void SpinCycle(uint32_t *pa) {
+  uint32_t regs[] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint32_t *ip = pa, *pa0 = pa;
+  uint32_t tmp;
 
-  unsigned int byte_code, op;
-  unsigned int a, b, c;
-  unsigned char ch;
-  unsigned int last_ldp = 0;
+  uint32_t byte_code, op;
+  uint32_t a, b, c;
+  uint8_t ch;
 
-  collection = (unsigned int *) calloc(MEMO_BUF_SIZE, sizeof(unsigned int *));
-  collection[0] = pa;
-
-  while (ip <= collection[0][0]) {
-    byte_code = collection[0][ip + 1];
+  while (1) {
+    byte_code = *ip;
     op = (byte_code & OP_MASK);
     a = (byte_code & RA_MASK) >> 6;
     b = (byte_code & RB_MASK) >> 3;
@@ -124,29 +119,19 @@ void SpinCycle(unsigned int *pa) {
         }
         break;
       case OP_ARI:
-        regs[a] = collection[regs[b]][regs[c] + 1];
+        regs[a] = (regs[b] == 0 ? pa0 : (uint32_t*)regs[b])[regs[c]];
         break;
       case OP_ARA:
-        /* Copy-on-write */
-        if (last_ldp != 0) {
-          collection[0] = malloc(
-              ((collection[0][0] + 1) * sizeof(unsigned int)));
-          memcpy(
-              collection[0],
-              collection[last_ldp],
-              (collection[last_ldp][0] + 1) * sizeof(unsigned int));
-          last_ldp = 0;
-        }
-        collection[regs[a]][regs[b] + 1] = regs[c];
+        (regs[a] == 0 ? pa0 : (uint32_t*)regs[a])[regs[b]] = regs[c];
         break;
       case OP_ADD:
-        regs[a] = regs[b] + regs[c];
+        regs[a] = (regs[b] + regs[c]) % 0x100000000;  /* 2^32 */
         break;
       case OP_MUL:
-        regs[a] = regs[b] * regs[c];
+        regs[a] = (regs[b] * regs[c]) % 0x100000000;  /* 2^32 */
         break;
       case OP_DIV:
-        regs[a] = regs[b] / regs[c];
+        regs[a] = (regs[b] % 0x100000000) / (regs[c] % 0x100000000);
         break;
       case OP_NOT:
         regs[a] = ~(regs[b] & regs[c]);
@@ -155,48 +140,34 @@ void SpinCycle(unsigned int *pa) {
         return;
         break;
       case OP_ALC:
-        /* TODO: implement more robust/less naive alloc and free */
-        /* Search from last known lower bound on free cell */
-        while (collection[pp_lb] != NULL) {
-          pp_lb++;
-        }
-        pp = pp_lb;
-        collection[pp] = calloc((regs[c] + 1), sizeof(unsigned int));
-        collection[pp][0] = regs[c];
-        regs[b] = pp;
+        tmp = regs[c];
+        regs[b] = (uint32_t)&((
+            (uint32_t*)calloc((regs[c] + 1), sizeof(uint32_t)))[1]);
+        ((uint32_t*)regs[b])[-1] = tmp;
         break;
       case OP_ABD:
-        /* make sure we're not shadowing it */
-        if (regs[c] != last_ldp) {
-          free(collection[regs[c]]);
-        } else {
-          /* this memory is now exclusively collection[0] */
-          last_ldp = 0;
-        }
-        collection[regs[c]] = NULL;
-        /* Keep track of known lower bound */
-        if (regs[c] < pp_lb) {
-          pp_lb = regs[c];
-        }
-        if (regs[c] == pp) {
-          pp--;
-        }
+        free(&(((uint32_t*)regs[c])[-1]));
         break;
       case OP_OUT:
-        putchar((unsigned char)regs[c]);
+        putchar((uint8_t)regs[c]);
         break;
       case OP_INP:
         regs[c] = ((ch = getchar()) != EOF) ? ch : 0xFF;
         break;
       case OP_LDP:
-        /* subtract 1 since we'll increment below */
-        ip = regs[c] - 1;
-        /* Shadow the copied memory to speed up "copy" */
-        if (regs[b] != 0) {
-          free(collection[0]);
-          collection[0] = collection[regs[b]];
-          last_ldp = regs[b];
+        /* NOTE: 0 refers to the special Platter Array 0 */
+        /* don't bother copy if we're jumping within the same array */
+        if (regs[b] != 0 && ((uint32_t*)regs[b]) != pa0) {
+          free(&(pa0[-1]));
+          pa0 = malloc(((((uint32_t*)regs[b])[-1] + 1) * sizeof(uint32_t)));
+          memcpy(
+              pa0,
+              &(((uint32_t*)(regs[b]))[-1]),
+              ((((uint32_t*)regs[b])[-1] + 1) * sizeof(uint32_t)));
+          pa0 = &(pa0[1]);
         }
+        /* subtract 1 since we'll increment below */
+        ip = (pa0 + regs[c] - 1);
         break;
       case OP_ORT:
         regs[(byte_code & OA_MASK) >> 25] = (byte_code & OV_MASK);
@@ -208,7 +179,9 @@ void SpinCycle(unsigned int *pa) {
     ip++;
   }
 
-  free(collection);
+  /* free the original platter array */
+  /* TODO(cskau): Is this a good idea? (Double free, not our memory, ..) */
+  free(&(pa[-1]));
 }
 
 int main(int argc, char *argv[]) {
