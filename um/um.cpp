000e744e66b27c4fa896b62b9972c5d0a143f82f
Clement Skau <clementskau@gmail.com>
2012-02-27 17:03:20 +0100

    Adds block compilation

--- um/um.cpp
+++ um/um.cpp
@@ -148,33 +148,48 @@ NativeCode compile() {
   /* TODO(cskau): maybe simply instanciate and store globally ? */
   Assembler assembler(Isolate::Current(), NULL, 0);
   CodeDesc desc;
-  size_t allocated;
   Label skip;
-  switch (op) {
-    case OP_IFM:
-      __ mov(eax, ExternalOperand(&reg[c]));
-      __ test(eax, eax);
-      __ j(zero, &skip, Label::kNear);
-      if (b != c) {
+  size_t allocated;
+  uint32_t ip_tmp = ip;
+  while (ip_tmp < byte_code[0]) {
+    op = (byte_code[(ip_tmp + 1)] & OP_MASK);
+    a = (byte_code[(ip_tmp + 1)] & RA_MASK) >> 6;
+    b = (byte_code[(ip_tmp + 1)] & RB_MASK) >> 3;
+    c = (byte_code[(ip_tmp + 1)] & RC_MASK);
+    switch (op) {
+      case OP_IFM:
+        /* since we're looping we need to clear label every round */
+        skip = Label();
+        __ mov(eax, ExternalOperand(&reg[c]));
+        __ test(eax, eax);
+        __ j(zero, &skip, Label::kNear);
+        if (b != c) {
+          __ mov(eax, ExternalOperand(&reg[b]));
+        }
+        __ mov(ExternalOperand(&reg[a]), eax);
+        __ bind(&skip);
+        break;
+      case OP_ADD:
         __ mov(eax, ExternalOperand(&reg[b]));
-      }
-      __ mov(ExternalOperand(&reg[a]), eax);
-      __ bind(&skip);
-      break;
-    case OP_ADD:
-      __ mov(eax, ExternalOperand(&reg[b]));
-      if (b == c) {
-        __ add(eax, eax);
-      } else {
-        __ add(eax, ExternalOperand(&reg[c]));
-      }
-      __ mov(ExternalOperand(&reg[a]), eax);
-      break;
-    default:
-      return NULL;
+        if (b == c) {
+          __ add(eax, eax);
+        } else {
+          __ add(eax, ExternalOperand(&reg[c]));
+        }
+        __ mov(ExternalOperand(&reg[a]), eax);
+        break;
+      default:
+        if (assembler.pc_offset() == 0) {
+          /* if we can't compile even the first instruction .. */
+          return NULL;
+        }
+        goto finish; /* Yippee-ki-yay ! */
+        break;
+    }
+    __ add(ExternalOperand(&ip), Immediate(1)); /* increment ip */
+    ip_tmp++;
   }
-  /* always needed ? */
-  __ add(ExternalOperand(&ip), Immediate(1));
+  finish:
   __ ret(0);
   // assembler.Print();
   assembler.GetCode(&desc);
